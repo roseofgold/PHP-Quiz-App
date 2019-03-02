749db554e90007fa568c98a0f96a6570e0136f64
Melinda S <roseofgold@gmail.com>
2019-03-02 11:24:12 -0800

    Display random questions and answers

--- inc/quiz.php
+++ inc/quiz.php
@@ -18,22 +18,25 @@
 // Include questions
 include 'questions.php';
 
-// Keep track of which questions have been asked
-$questionsAsked=array();
-var_dump ($questionsAsked);
-
 // Show which question they are on
 $totalQuestions = 10;
-$currentQuestion = filter_input(INPUT_GET,'currentQuestion',FILTER_SANITIZE_NUMBER_INT);
-$currentQuestion = 1;
+$currentQuestion = filter_input(INPUT_GET,'q',FILTER_SANITIZE_NUMBER_INT);
+if (empty($currentQuestion)){
+	$currentQuestion = 1;
+}
+
+// Keep track of which questions have been asked
+$questionAsked[1]=filter_input(INPUT_POST,$_SESSION['questions'][1],FILTER_SANITIZE_NUMBER_INT);
 
 // Show random question
+$randomQuestion=rand(0,count($questions));
 
 // Shuffle answer buttons
 
 
 // Toast correct and incorrect answers
 // Keep track of answers
+$answeredQuestions[]=array();
 // If all questions have been asked, give option to show score
 // else give option to move to next question
 
