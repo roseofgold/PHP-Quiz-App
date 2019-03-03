62e06caa465ddab0120d9da4ec26e48c2fff54a7
Melinda S <roseofgold@gmail.com>
2019-03-03 06:24:34 -0800

    Revert "Correct questions are counted"
    This reverts commit d3fd6f73235ef059dace481265f0c0a0c97c82ed.

--- inc/quiz.php
+++ inc/quiz.php
@@ -22,74 +22,22 @@ include 'questions.php';
 $totalQuestions = 10;
 $currentQuestion = filter_input(INPUT_GET,'q',FILTER_SANITIZE_NUMBER_INT);
 if (empty($currentQuestion)){
-	session_destroy();
 	$currentQuestion = 1;
 }
 
-//Keep track of total questions answered and correctly.
-$questionsAnswered = 0;
-if(!isset($_SESSION['questionsAnsweredCorrectly'])){
-	$_SESSION['questionsAnsweredCorrectly']=0;
-}
-
 // Keep track of which questions have been asked
-$questionAsked[]=array_push($randomQuestion);
+$questionAsked[1]=filter_input(INPUT_POST,$_SESSION['questions'][1],FILTER_SANITIZE_NUMBER_INT);
+
+// Show random question
+$randomQuestion=rand(0,count($questions));
+
+// Shuffle answer buttons
 
 
-function printQuestion($array,$currentQuestion){
-	// get question number
-	$questionNumber = $currentQuestion;
-	
-	// Choose a number between 0 and the number of elements in the array.
-	$randomArrayNumber = array_rand($array,1);
-
-	// Call the Random Question!
-	$choseQuestion = $array[$randomArrayNumber];
-	
-	// Ask the question
-	$chosenQuestion = '<p class="quiz">What is ' 
-		. $choseQuestion['leftAdder'] 
-		. ' + ' 
-		. $choseQuestion['rightAdder']
-		. "?</p>";	
-	
-	// Print the question
-	echo $chosenQuestion;
-	
-	// Shuffle answer buttons
-	$possibleAnswers = [$choseQuestion['correctAnswer'],$choseQuestion['firstIncorrectAnswer'],$choseQuestion['secondIncorrectAnswer']];
-	shuffle($possibleAnswers);
-	
-	//Print possible answers
-	echo '<form action="index.php?q=' . ($questionNumber+1) . '" method="post">';
-	echo '<input type="hidden" name="correctAnswer" value="'. $choseQuestion['correctAnswer'] .	'" />';
-	foreach ($possibleAnswers as $answer) {
-		echo '<input type="submit" class="btn" name="answer" value="';
-		echo $answer;
-		echo '" />';
-	}
-	echo '</form>';
-}
-
-if(isset($_POST['answer'])){
-	$yourAnswer=filter_input(INPUT_POST,'answer',FILTER_SANITIZE_NUMBER_INT);
-	$correctAnswer=filter_input(INPUT_POST,'correctAnswer',FILTER_SANITIZE_NUMBER_INT);
-
-	// Toast correct and incorrect answers
-	if($yourAnswer == $correctAnswer){
-		echo '<h1>You got the correct answer</h1>';
-		$_SESSION['questionsAnsweredCorrectly']++;
-	} else{
-		echo '<h1>Sorry that was not the correct answer</h1>';
-	}
-}
-
+// Toast correct and incorrect answers
 // Keep track of answers
+$answeredQuestions[]=array();
 // If all questions have been asked, give option to show score
-if ($currentQuestion==11){
-			echo '<h2>You\'ve answered ' . $_SESSION['questionsAnsweredCorrectly'] . ' question(s) correctly!</h2>';
- 
-}
 // else give option to move to next question
 
 
--- index.php
+++ index.php
@@ -13,9 +13,17 @@ include 'inc/quiz.php';
 <body>
     <div class="container">
         <div id="quiz-box">
-<?php if ($currentQuestion <= $totalQuestions){?>		
+<?php 
+if ($currentQuestion <= $totalQuestions){
+?>		
             <p class="breadcrumbs">Question <?php echo $currentQuestion ?> of <?php echo $totalQuestions?></p>
-            <?php printQuestion($questions,$currentQuestion); ?>            
+            <p class="quiz">What is <?php echo $questions[$randomQuestion]['leftAdder']?> + <?php echo $questions[$randomQuestion]['rightAdder']?>?</p>
+            <form action="index.php?q=<?php echo $currentQuestion+1; ?>" method="post">
+                <input type="hidden" name="id" value="<?php echo $randomQuestion; ?>" />
+                <input type="submit" class="btn" name="answer" value="<?php echo $questions[$randomQuestion]['correctAnswer']; ?>" />
+                <input type="submit" class="btn" name="answer" value="<?php echo $questions[$randomQuestion]['firstIncorrectAnswer']; ?>" />
+                <input type="submit" class="btn" name="answer" value="<?php echo $questions[$randomQuestion]['secondIncorrectAnswer']; ?>" />
+            </form>
         </div>
 <?php } ?>
     </div>
