928806610cea8361f8447346aa2cccbdb6a90f6f
Melinda S <roseofgold@gmail.com>
2019-03-04 22:12:48 -0800

    Show placeholder for correct answers at the correct time.

--- index.php
+++ index.php
@@ -13,15 +13,15 @@ include 'inc/quiz.php';
 <body>
     <div class="container">
         <div id="quiz-box">
-<?php if($_SESSION['currentQuestion'] <= $_SESSION['totalQuestions']){?>
-            <p class="breadcrumbs">Question <?php echo $_SESSION['currentQuestion']; ?> of <?php echo $_SESSION['totalQuestions'];?></p>
+<?php if($_SESSION['currentQuestion'] == $_SESSION['totalQuestions']){?>
+            <p>Congrats! You got xx questions right!</p>
+<?php } else { ?>
+			<p class="breadcrumbs">Question <?php echo $_SESSION['currentQuestion']; ?> of <?php echo $_SESSION['totalQuestions'];?></p>
             <p class="quiz">What is <?php echo $leftAdder; ?> + <?php echo $rightAdder; ?>?</p>
             <form action="index.php?q=<?php echo $_SESSION['currentQuestion']+1; ?>" method="post">
                 <input type="hidden" name="id" value="0" />
                 <?php echo $possibleAnswers; ?>
             </form>
-<?php } else { ?>
-			<p>Congrats! You got xx questions right!</p>
 <?php } ?>
         </div>
     </div>
