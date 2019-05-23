7fcd75b00cd8d19b5aa183e8eabf40e8522f177a
Melinda <roseofgold@gmail.com>
2019-05-23 13:24:54 -0700

    remove unnecessary comments

--- inc/quiz.php
+++ inc/quiz.php
@@ -1,18 +1,6 @@
 <?php
 /*
  * PHP Techdegree Project 2: Build a Quiz App in PHP
- *
- * These comments are to help you get started.
- * You may split the file and move the comments around as needed.
- *
- * You will find examples of formating in the index.php script.
- * Make sure you update the index file to use this PHP script, and persist the users answers.
- *
- * For the questions, you may use:
- *  1. PHP array of questions
- *  2. json formated questions
- *  3. auto generate questions
- *
  */
 
 // Include questions
@@ -79,9 +67,3 @@ if(isset($studentAnswer)){
 	}
 	$toast .= '</div>';
 }
-
-// If all questions have been asked, give option to show score
-// else give option to move to next question
-
-
-// Show score
