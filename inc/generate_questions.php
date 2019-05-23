9870d5bd8e522d841126f1c3170c32af3a2f1fb7
Melinda <roseofgold@gmail.com>
2019-05-23 13:33:48 -0700

    Update comments

--- inc/generate_questions.php
+++ inc/generate_questions.php
@@ -1,20 +1,19 @@
 <?php
 function generateAdvancedQuestions(){
-	// Generate random questions
+	// set array to hold questions
 	$randomNumbers = array();
 
 	// Loop for required number of questions
 	for($i=0;$i<10;$i++){
 
-		// Get random numbers to add
+		// Get two random numbers
 		$randNum1 = rand(1,99);
 		$randNum2 = rand(1,99);
 
 		// Calculate correct answer
 		$correct = $randNum1 + $randNum2;
 
-		// Get incorrect answers within 10 numbers either way of correct answer
-		// Make sure it is a unique answer
+		// Get two, unique, incorrect answers within 10 numbers either way of correct answer
 		$incorrect1 = rand(($correct-10),($correct+10));
 		while ($incorrect1 == $correct){
 			$incorrect1 = rand(($correct-10),($correct+10));
@@ -26,7 +25,7 @@ function generateAdvancedQuestions(){
 		}
 
 
-		// Add question and answer to questions array
+		// Add random numbers and answers to questions array
 		$randomNumbers[]=[
 			"leftAdder" => $randNum1,
 			"rightAdder" => $randNum2,
@@ -36,7 +35,7 @@ function generateAdvancedQuestions(){
 		];
 	}
 
-	//write results to json file
+	// Write results to json file
 	$fp = fopen('inc/questions.json', 'w');
 	fwrite($fp, json_encode($randomNumbers));
 	fclose($fp);
